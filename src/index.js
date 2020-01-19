042afbd4a0bf8888b7925286a8e91f02e4c53887
Mohammed Raja <mohammed2raja@gmail.com>
2020-01-20 02:12:35 +0530

    changing file bame

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function drawAreaChart(option) {
   const maxYAxisValue = option.maxYvalue;
   const highlightValue1 = option.highlightHline1;
   const highlightValue2 = option.highlightHline2;
-  
+
   const svgContainer = container
     .append("svg")
     .attr(
@@ -41,11 +41,11 @@ function drawAreaChart(option) {
         margin.top +
         margin.bottom}`
     );
-  
+
   const svgCanvas = svgContainer
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
-  
+
   const data = option.data;
 
   console.log(d3.max(data, d => d.value));
@@ -62,13 +62,13 @@ function drawAreaChart(option) {
       .attr("id", "y-axis")
       .attr("class", "axis")
       .call(d3.axisLeft(yScale));
-  
+
   /* X axis */
   const xScale = d3
     .scaleUtc()
     .domain(d3.extent(data, d => d.date))
     .range([margin.left, width - margin.right]);
-  
+
   const xAxis = g =>
     g
       .attr("id", "x-axis")
@@ -87,7 +87,7 @@ function drawAreaChart(option) {
       .attr("transform", function(d) {
         return "rotate(-65)";
       });
-  
+
   var areaGradient = svgContainer
     .append("defs")
     .append("linearGradient")
@@ -96,7 +96,7 @@ function drawAreaChart(option) {
     .attr("y1", "0%")
     .attr("x2", "0%")
     .attr("y2", "100%");
-  
+
   areaGradient
     .append("stop")
     .attr("offset", "0%")
@@ -107,9 +107,9 @@ function drawAreaChart(option) {
     .attr("offset", "80%")
     .attr("stop-color", "white")
     .attr("stop-opacity", 0);
-  
+
   const curve = d3.curveLinear;
-  
+
   const line = d3
     .line()
     // .curve() allows to specify the behavior of the line itself
@@ -121,44 +121,42 @@ function drawAreaChart(option) {
     .append("path")
     .attr("class", "line")
     .attr("d", line(data));
-  
+
   const area = d3
     .area()
     .curve(curve)
     .x(d => xScale(d.date))
     .y0(yScale(minYAxisValue))
     .y1(d => yScale(d.value));
-  
+
   svgCanvas
     .append("path")
     .datum(data)
     .attr("class", "area")
     .attr("d", area);
-  
+
   svgCanvas.append("g").call(yAxis);
   svgCanvas.append("g").call(xAxis);
-  
+
   // GRID system
   // target all the horizontal ticks and include line elements making up vertical grid lines
-  d3
-    .selectAll(".container #x-axis g.tick")
+  d3.selectAll(".container #x-axis g.tick")
     .append("line")
     .attr("class", "grid-line")
     .attr("x1", 0)
     .attr("x2", 0)
     .attr("y1", 0)
     .attr("y2", -(height - (margin.top + margin.bottom)));
-  
+
   // repeat the operation, but with regards to horizontal grid lines
-  d3
-    .selectAll(".container #y-axis g.tick")
+  d3.selectAll(".container #y-axis g.tick")
     .append("line")
     .attr("class", "grid-line")
     .attr("x1", 0)
     .attr("x2", width - (margin.left + margin.right))
     .attr("y1", 0)
     .attr("y2", 0);
-  
+
   const drawMajorLine = value => {
     const INNER_HEIGHT = height - (margin.top + margin.bottom);
     const AVAILABLE_HEIGHT = maxYAxisValue - minYAxisValue;
@@ -175,56 +173,61 @@ function drawAreaChart(option) {
   };
   drawMajorLine(highlightValue1);
   drawMajorLine(highlightValue2);
-  
-  
-  
+
   function hoverMouseOn() {
     var mouse_x = d3.mouse(this)[0];
     var mouse_y = d3.mouse(this)[1];
     var graph_y = yScale.invert(mouse_y);
     var graph_x = xScale.invert(mouse_x);
-    if(mouse_x <= margin.left || mouse_x >= width - margin.right || mouse_y >= height - margin.bottom){
-      return
+    if (
+      mouse_x <= margin.left ||
+      mouse_x >= width - margin.right ||
+      mouse_y >= height - margin.bottom
+    ) {
+      return;
     }
-  
-  
+
     hoverLine.attr("x1", mouse_x).attr("x2", mouse_x);
     hoverLineGroup.style("opacity", 1);
-  
+
     var mouseDate = xScale.invert(mouse_x);
     const bisect = d3.bisector(function(d) {
       return d.date;
     }).right;
     const idx = bisect(data, graph_x);
-  
+
     var d0 = data[idx - 1];
     var d1 = data[idx];
     if (d0 && d1) {
       var d = mouseDate - d0[0] > d1[0] - mouseDate ? d1 : d0;
-      tooltipsContr.attr('transform', `translate(${mouse_x + 5},${yScale(d.value)})`)
+      tooltipsContr.attr(
+        "transform",
+        `translate(${mouse_x + 5},${yScale(d.value)})`
+      );
       tooltipsContr.style("opacity", "1");
-      tooltipsText.attr('x', '5.5em');
-      tooltipsText.attr('y', '-0.3em');
-      tooltipsText.text(d3.utcFormat("%b %d, %I:%M %p")(d.date)); 
-      tooltipsTemp.attr('x', '3em');
-      tooltipsTemp.attr('y', '1em');
-      tooltipsTemp.text(d.value); 
-  
+      tooltipsText.attr("x", "5.5em");
+      tooltipsText.attr("y", "-0.3em");
+      tooltipsText.text(d3.utcFormat("%b %d, %I:%M %p")(d.date));
+      tooltipsTemp.attr("x", "3em");
+      tooltipsTemp.attr("y", "1em");
+      tooltipsTemp.text(d.value);
+
       cle
-      .attr('cx', mouse_x)
-      .attr('cy', yScale(d.value))
-      .style("opacity", "1")
+        .attr("cx", mouse_x)
+        .attr("cy", yScale(d.value))
+        .style("opacity", "1");
     }
   }
   function hoverMouseOff(d) {
     hoverLineGroup.style("opacity", 1e-6);
   }
-  
+
   //Line chart mouse over
-  const hoverLineGroup = svgCanvas.append("g")
-  .attr("class", "hover-line")
-  .style("opacity", 0);
-  
+  const hoverLineGroup = svgCanvas
+    .append("g")
+    .attr("class", "hover-line")
+    .style("opacity", 0);
+
   const hoverLine = hoverLineGroup
     .append("line")
     .attr("stroke", "rgba(0, 0, 0, 0.5)")
@@ -233,40 +236,42 @@ function drawAreaChart(option) {
     .attr("x2", 0)
     .attr("y1", margin.top)
     .attr("y2", height - margin.bottom);
-  
-    /* tooltips */
-  const tooltipsContr = hoverLineGroup.append('g');
+
+  /* tooltips */
+  const tooltipsContr = hoverLineGroup.append("g");
   tooltipsContr.style("opacity", "0");
-  tooltipsContr.append('path')
-    .style('fill', 'white').style('stroke', 'black')
-    .attr('d', rightTooltipPath(70,30,10,5))
-  
-  const tooltipsText = tooltipsContr.append('text')
-    .attr('text-anchor', 'middle')
-    .attr('class', 'tooltips-text');
-  
-  const tooltipsTempCntr = tooltipsContr.append('text');
-  const tooltipsTemp = tooltipsTempCntr.append('tspan')
-    .attr('text-anchor', 'middle')
-    .attr('class', 'tooltips-text tooltips-temp');
-  
-  
-  
-  
-  var cle = hoverLineGroup.append("circle")
+  tooltipsContr
+    .append("path")
+    .style("fill", "white")
+    .style("stroke", "black")
+    .attr("d", rightTooltipPath(70, 30, 10, 5));
+
+  const tooltipsText = tooltipsContr
+    .append("text")
+    .attr("text-anchor", "middle")
+    .attr("class", "tooltips-text");
+
+  const tooltipsTempCntr = tooltipsContr.append("text");
+  const tooltipsTemp = tooltipsTempCntr
+    .append("tspan")
+    .attr("text-anchor", "middle")
+    .attr("class", "tooltips-text tooltips-temp");
+
+  var cle = hoverLineGroup
+    .append("circle")
     .style("opacity", "0")
     .attr("stroke", "#f00")
     .attr("stroke-width", "1")
     .attr("fill", "#000")
     .attr("r", 4);
-  
+
   svgCanvas.on("mouseout", hoverMouseOff).on("mousemove", hoverMouseOn);
-  
+
   function rightTooltipPath(width, height, offset, radius) {
-    const left = offset
-    const right = offset + width
-    const top = -height / 2
-    const bottom = height / 2
+    const left = offset;
+    const right = offset + width;
+    const top = -height / 2;
+    const bottom = height / 2;
     return `M 0,0 
       L ${left},${-offset} 
       V ${top + radius}
@@ -278,9 +283,8 @@ function drawAreaChart(option) {
       H ${left + radius}
       Q ${left},${bottom} ${left},${bottom - radius}
       V ${offset} 
-      L 0,0 z`
+      L 0,0 z`;
   }
-  
 }
 
 // container
@@ -345,14 +349,14 @@ temperatureDate = temperatureDate.map(d => {
 });
 
 drawAreaChart({
-  container: '.container', 
+  container: ".container",
   width: 600,
   height: 300,
-  data: temperatureDate, 
-  Xlable: 'date', 
-  Ylabel: 'value',
+  data: temperatureDate,
+  Xlable: "date",
+  Ylabel: "value",
   minYvalue: 90,
   maxYvalue: 105, //d3.max(data, d => d.value),
   highlightHline1: 95,
   highlightHline2: 103
-})
+});
